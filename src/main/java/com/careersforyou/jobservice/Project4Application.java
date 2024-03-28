ae4cd03dc8991561ff9c92810959c217c4b7efdb
ppatria <prodeopatria51@gmail.com>
2024-03-27 21:59:48 -0600

    yuh

---
+++ src/main/java/com/careersforyou/jobservice/Project4Application.java
@@ -0,0 +1,13 @@
+package com.careersforyou.jobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class project4application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(project4application.class, args);
+	}
+
+}
